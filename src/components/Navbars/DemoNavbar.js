eb176954cce9137dfcf45f582a63a4ca72a53842
sehoon <kyung6805@naver.com>
2025-01-14 16:58:38 +0900

    navigation+mozit

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -75,7 +75,7 @@ class DemoNavbar extends React.Component {
               <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
                 <img
                   alt="..."
-                  src={require("assets/img/brand/mozit1.png")}
+                  src={require("assets/img/brand/mozit.png")}
                 />
               </NavbarBrand>
               <button className="navbar-toggler" id="navbar_global">
@@ -94,7 +94,7 @@ class DemoNavbar extends React.Component {
                       <Link to="/">
                         <img
                           alt="..."
-                          src={require("assets/img/brand/mozit1.png")}
+                          src={require("assets/img/brand/mozit.png")}
                         />
                       </Link>
                     </Col>
--- src/index.js
+++ src/index.js
@@ -35,11 +35,11 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<Index />} />
-      {/* <Route path="/landing-page" exact element={<Landing />} />
+      <Route path="/landing-page" exact element={<Landing />}/>
       <Route path="/login-page" exact element={<Login />} />
-      <Route path="/profile-page" exact element={<Profile />} />
-      <Route path="/register-page" exact element={<Register />} />
-      <Route path="*" element={<Navigate to="/" replace />} /> */}
+      <Route path="/profile-page" exact element={<Profile />}/>
+      <Route path="/register-page" exact element={<Register />}/>
+      <Route path="*" element={<Navigate to="/" replace />} /> 
     </Routes>
   </BrowserRouter>
 );
